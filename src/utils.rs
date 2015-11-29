0a697f5cf954dea53a1d9ecd516c6968d7ffd9b6
Aleksey Kuznetsov <zummenix@gmail.com>
2015-11-29 20:31:41 +0500

    Combine requests extensions into one trait.

--- src/utils.rs
+++ src/utils.rs
@@ -4,33 +4,23 @@ use router::Router;
 use urlencoded::UrlEncodedQuery;
 
 /// A convenient extenstion for `Request`.
-///
-/// Returns string for router parameter.
-pub trait GetParam {
-    /// Returns string for router parameter or empty string if parameter is not
-    /// exist.
+pub trait IronRequestExt {
+    /// Returns path parameter or empty string if parameter is not exist.
     ///
     /// # Panics
-    /// Panics if `Router` is not presented in request's extensions.
-    fn param(&self, key: &str) -> &str;
+    /// Panics if a `Router` has not been registered with Iron.
+    fn path_param(&self, key: &str) -> &str;
+
+    /// Returns query parameter or empty string if parameter is not exist.
+    fn query_param(&mut self, key: &str) -> &str;
 }
 
-impl<'a, 'b> GetParam for Request<'a, 'b> {
-    fn param(&self, key: &str) -> &str {
+impl<'a, 'b> IronRequestExt for Request<'a, 'b> {
+    fn path_param(&self, key: &str) -> &str {
         self.extensions.get::<Router>().unwrap().find(key).unwrap_or("")
     }
-}
 
-/// A convenient extenstion for `Request`.
-///
-/// Returns string for query key.
-pub trait GetQuery {
-    /// Returns string for query key or empty string if key isn't exist.
-    fn query(&mut self, key: &str) -> &str;
-}
-
-impl<'a, 'b> GetQuery for Request<'a, 'b> {
-    fn query(&mut self, key: &str) -> &str {
+    fn query_param(&mut self, key: &str) -> &str {
         self.get_ref::<UrlEncodedQuery>().ok()
             .and_then(|m| m.get(key).and_then(|v| v.first()))
             .map_or("", |s| s.as_ref())
--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -6,10 +6,10 @@ use error::*;
 use utils::*;
 
 pub fn get_user_handler(r: &mut Request) -> IronResult<Response> {
-    let id = try!(r.param("id").parse::<i32>().map_err(|_| bad_request("Could not parse id.")));
+    let id = try!(r.path_param("id").parse::<i32>().map_err(|_| bad_request("Could not parse id.")));
     Ok(Response::with((status::Ok, id.to_string())))
 }
 
 pub fn get_users_handler(r: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, r.query("search"))))
+    Ok(Response::with((status::Ok, r.query_param("search"))))
 }
